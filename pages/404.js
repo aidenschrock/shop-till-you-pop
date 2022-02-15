d5875f8ce33685be065deaa0cf98b35b6381a8d9
Aiden Schrock <aidennoelschrock@gmail.com>
2022-02-14 23:14:34 -0600

    feat: add 404 animation

--- pages/404.js
+++ pages/404.js
@@ -1,18 +1,26 @@
 import { Typography, Button } from "@mui/material";
 import { useRouter } from "next/router";
+import Lottie from "react-lottie";
+import animation from "../lotties/404Animation.json";
 
 function Page404() {
   const router = useRouter();
 
+  const defaultOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: animation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
   return (
     <div style={style.container}>
-      <Typography variant="h1">404</Typography>
-      <Typography variant="h5">Uh Oh! There&apos;s nothing here!</Typography>
-      {/* <img
-        style={style.image}
-        src={page404Image}
-        alt="man exiting portal spooks alien"
-      /> */}
+      <Typography variant="h3">Uh Oh! There&apos;s nothing here!</Typography>
+      <div>
+        <Lottie options={defaultOptions} />
+      </div>
 
       <Button
         sx={{ marginTop: "3 %" }}
@@ -32,6 +40,8 @@ const style = {
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
+    margin: "2%",
+    textAlign: "center",
   },
   image: {
     height: "400px",

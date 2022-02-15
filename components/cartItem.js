b600c031a54111084fd51357a1b476e5828345fb
Aiden Schrock <aidennoelschrock@gmail.com>
2022-02-14 23:24:47 -0600

    fix: remove image import

--- components/cartItem.js
+++ components/cartItem.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import apple from "../public/apple.png";
 import Image from "next/image";
 import { Typography, Button, Grid, IconButton } from "@mui/material";
 import ClearIcon from "@mui/icons-material/Clear";
